2ce58a835bfba10bd21c90f7ec1af9ebdadcc8b6
AdityaManojA <130238815+AdityaManojA@users.noreply.github.com>
2024-11-24 14:36:07 +0530

    Create foreach.php

---
+++ foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <?php
+    $user_valid=1;
+    $array=array("1","2","3","4","5","6");
+    foreach($array as $value){
+        echo "Array is {$value}<br>"
+    }
+    ?>
+</body>
+</html>
