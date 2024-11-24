19d7914c0855628bfbdd59efe5f0d1e33efd5167
AdityaManojA <130238815+AdityaManojA@users.noreply.github.com>
2024-11-24 18:42:17 +0530

    Create implode.php

---
+++ implode.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <?php
+    $array=array("A", "B", "C", "D", "E", "F");
+    $implodedarray=implode("-",$array);
+    ?>
+</body>
+</html>
