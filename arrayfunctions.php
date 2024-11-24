1a37b1da1eccbbcc155ec5dd7621db23940e0146
AdityaManojA <130238815+AdityaManojA@users.noreply.github.com>
2024-11-24 19:38:38 +0530

    Create arrayfunctions.php

---
+++ arrayfunctions.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello</h1>
+    <?php
+    $array1=[1,2,3];
+    $array2=[4,5,6];
+    $array3=array_merge($array1,$array2);
+    foreach($array3 as $value){
+        echo "Value is {$value}";
+    }
+    ?>
+</body>
+</html>
