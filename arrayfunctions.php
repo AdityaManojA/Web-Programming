890b9c333e3ff3fdcf9ab591e64c406c27822027
AdityaManojA <130238815+AdityaManojA@users.noreply.github.com>
2024-11-24 19:42:51 +0530

    Update arrayfunctions.php

--- arrayfunctions.php
+++ arrayfunctions.php
@@ -11,6 +11,7 @@
     $array1=[1,2,3];
     $array2=[4,5,6];
     $array3=array_merge($array1,$array2);
+    array_push($array3,7,8,9);
     foreach($array3 as $value){
         echo "Value is {$value}";
     }
