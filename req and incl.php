10fc3af266237ab634433bda648e5261bcd0b3ec
AdityaManojA <130238815+AdityaManojA@users.noreply.github.com>
2024-11-25 13:24:32 +0530

    Create req and incl.php

---
+++ req and incl.php
@@ -0,0 +1,9 @@
+<?php
+// Using require for a mandatory file
+require "database.php"; 
+
+// Using include for optional functionality
+include "header.php";
+
+echo "The script continues here.";
+?>
