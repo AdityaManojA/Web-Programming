fe3a5a6f8652f726c7bf4d275770c1b9bbc0c854
AdityaManojA <130238815+AdityaManojA@users.noreply.github.com>
2024-11-25 17:36:25 +0530

    Update cookie.php

--- cookie.php
+++ cookie.php
@@ -3,3 +3,16 @@ $cookie_name='user';
 $cookie_value= 'John Smith';
 setcookie($cookie_name,$cookie_value,time()+ (86400*30),"/");//30 days cookie storage
 ?>
+
+<html>
+    <body>
+        <?php
+        if(isset($_COOKIE[$cookie_name])){
+            echo "Welcome ". $_COOKIE[$cookie_name]. "!<br>";
+        }
+        else {
+            echo "Cookie not found";
+        }
+        ?>
+    </body>
+</html>
